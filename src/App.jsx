418c73f31df301e78adc82978ab6057bd4ab9dea
Jesus Gallardo <gallardomoralesjesus@gmail.com>
2022-08-19 20:39:37 +0200

    feat: add Logic for favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { CompactPicker } from 'react-color'
 import './App.css'
+import ColorList from './components/ColorsList/ColorList'
+import ColorsName from './components/ColorsName/ColorsName'
 import Header from './components/Header/Header'
+import SavedFavs from './components/SavedFavs/SavedFavs'
 
 function App () {
+  const [myColors, setMyColors] = useState({ name: 'test', colors: ['red', 'green', 'yellow', '', ''] })
+  const [selectedColor, setSelectedColor] = useState()
+  const [favorites, setFavorites] = useState(JSON.parse(localStorage.getItem('favColors') || '[]'))
+  const [selectedFav, setSelectedFav] = useState()
+
+  useEffect(() => {
+    localStorage.setItem('favColors', JSON.stringify(favorites))
+  }, [favorites])
+  useEffect(() => {
+    if (selectedFav) { setMyColors(selectedFav) }
+  }, [selectedFav])
+  const handleColor = (color) => {
+    const newColor = [...myColors.colors]
+    newColor[selectedColor] = color.hex
+    setMyColors({ name: myColors.name, colors: newColor })
+  }
+
   return (
     <div className="App">
       <Header />
+      <main>
+        <ColorList myColors={myColors} setSelectedColor={setSelectedColor} selectedFav={selectedFav}/>
+        <div className='options'>
+        <CompactPicker onChange={handleColor}/>
+        <ColorsName myColors={myColors} setMyColors={setMyColors} favorites={favorites} setFavorites={setFavorites}/>
+        </div>
+        <SavedFavs favorites={favorites} setFavorites={setFavorites} setSelectedFav={setSelectedFav}/>
+
+      </main>
     </div>
   )
 }
---
+++ src/components/SavedFavs/SavedFavs.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import bin from '../../assets/bin.svg'
+import styles from './styles.module.css'
+const SavedFavs = ({ favorites, setFavorites, setSelectedFav }) => {
+  const deleteFavorite = (i) => {
+    const newFavs = favorites.filter((fav, index) => index !== i)
+    setFavorites(newFavs)
+  }
+  const handleFav = (fav) => {
+    setSelectedFav(fav)
+  }
+  return (
+    <section className={styles.savedFavs}>
+      <h2>saved</h2>
+      <ul>
+        {
+          favorites.map((fav, i) => {
+            return (
+              <li key={i} onClick={() => handleFav(fav)}>
+              <div className={styles.title}>
+                <p>{fav.name}</p>
+                <button onClick={() => deleteFavorite(i)}>
+                  <img src={bin} alt="icon delete" />
+                </button>
+              </div>
+              <div className={styles.colorsFav}>
+                <ol>
+                  {
+                    fav.colors.map((favColor, i) => {
+                      return (
+                        <li key={i}>
+                          <div className={styles.color} style={{ background: `${favColor}` }}></div>
+                        </li>
+                      )
+                    })
+                  }
+                </ol>
+              </div>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </section>
+  )
+}
+
+export default SavedFavs
